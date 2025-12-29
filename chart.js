8a0986e6f065ad1797fbb4afd06567c4faf36c78
riverize <47478519+mobinogi@users.noreply.github.com>
2025-12-29 13:53:53 +0900

    Update chart.js

--- chart.js
+++ chart.js
@@ -193,7 +193,7 @@ function updateDashboardChart() {
         y: {
           // ✨ [핵심] grace: '10%' -> 데이터 최댓값보다 10% 더 높게 천장을 잡습니다.
           // 이렇게 하면 그래프가 꼭대기에 안 닿아서 말풍선이 뜰 공간이 생깁니다.
-          grace: '30%',
+          grace: '60%',
           
           grid: { color: 'rgba(200, 200, 200, 0.1)' },
           ticks: { color: '#9ca3af' },
